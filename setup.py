db04fcc5e13e0a2fc7a5bfce0cdf96f9d5e48fa8
Benjamin Delmée <benjamin.delmee@gmail.com>
2019-04-27 01:47:19 +0200

    Add setup.py to allow obstinate to be imported as a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='obstinate',
+    version='0.1',
+    description='Automaticaly replays a request when a connection error occurs',
+    url='https://github.com/benjamindelmee/obstinate-requests',
+    author='Benjamin Delmee',
+    author_email='benjamindelmee@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False
+)
