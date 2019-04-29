c3da29416a36e56d3f9b59beb3f28256196655c2
Benjamin Delmée <benjamin.delmee@gmail.com>
2019-04-29 13:30:18 +0200

    test: ensure that the correct number of retries is performed

--- test/Server.py
+++ test/Server.py
@@ -15,23 +15,40 @@ class Server(threading.Thread):
 
     def run(self):
         self.http_server = HTTPServer((self.host_name, self.port_number), Handler)
+        # add a counter to track the number of requests received
+        self.http_server.obst_counter = 0
         self.http_server.serve_forever()
 
     def shutdown(self):
         self.http_server.shutdown()
+    
+    def counter(self):
+        """Return the number of queries received since the last call to
+        the method `reset_counter`"""
+
+        return self.http_server.obst_counter
+
+    def reset_counter(self):
+        self.http_server.obst_counter = 0
 
 class Handler(BaseHTTPRequestHandler):
     """Router for the HTTP server"""
 
     def do_GET(self):
+        # increment the counter each time a request is received
+        self.server.obst_counter += 1
+
+        # handle the routes
         if self.path[:13] == '/status_code=':
             status = int(self.path[13:])
         else:
             status = 404
+        
+        # send the response
         self.send_response(status)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        
+
     # deactivate logs
     def log_message(self, *args, **kwargs):
         pass
--- test/test.py
+++ test/test.py
@@ -29,7 +29,7 @@ class Test(unittest.TestCase):
         cls.http_server.shutdown()
     
     def test_code_200(self):
-        """It should returns the server's respons when everthing works
+        """It should returns the server's response when everthing works
         fine"""
 
         url = 'http://localhost:7654/status_code=200'
@@ -38,9 +38,37 @@ class Test(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
 
-    def test_status_forcelist(self):
-        """It should return the server's respons after several queries
-        with status code 500"""
+    def test_status_forcelist_1(self):
+        """It should make several attemps when the status code received
+        isn't the one expected"""
+
+        url = 'http://localhost:7654/status_code=500'
+        
+        # start counting the number of requests received
+        self.http_server.reset_counter()
+
+        res = obstinate.oget(url, o_status_forcelist=['500'],
+            o_max_attempts=2)
+
+        self.assertEqual(3, self.http_server.counter())
+
+    def test_status_forcelist_2(self):
+        """It should not make several attemps when the status code
+        received wasn't listed in `o_status_forcelist`"""
+
+        url = 'http://localhost:7654/status_code=500'
+        
+        # start counting the number of requests received
+        self.http_server.reset_counter()
+
+        res = obstinate.oget(url, o_status_forcelist=['501'],
+            o_max_attempts=2)
+
+        self.assertEqual(1, self.http_server.counter())
+
+    def test_behavior_consistency_1(self):
+        """It should return the server's response after several queries
+        with status code 500, as the requests library would do"""
         
         url = 'http://localhost:7654/status_code=500'
         
@@ -48,7 +76,7 @@ class Test(unittest.TestCase):
         
         self.assertEqual(res.status_code, 500)
 
-    def test_behavior_consistance(self):
+    def test_behavior_consistency_2(self):
         """It should raise the same error as the requests library"""
 
         def f():
